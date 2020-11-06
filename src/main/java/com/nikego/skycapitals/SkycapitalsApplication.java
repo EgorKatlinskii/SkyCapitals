18aaf44459f4dbb799b5d03752310f229eb1c7af
Egor <egor.katlinskii@yandex.by>
2020-11-06 22:37:16 +0300

    save

--- src/main/java/com/nikego/skycapitals/SkycapitalsApplication.java
+++ src/main/java/com/nikego/skycapitals/SkycapitalsApplication.java
@@ -2,13 +2,10 @@ package com.nikego.skycapitals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.admin.SpringApplicationAdminMXBean;
 
 @SpringBootApplication
 public class SkycapitalsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SkycapitalsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SkycapitalsApplication.class, args);
+    }
 }
