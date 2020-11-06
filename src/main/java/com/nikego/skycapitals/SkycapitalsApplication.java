cc1d7479bc83e8d096ab6f5ef0c896bddf52e002
Egor <egor.katlinskii@yandex.by>
2020-11-06 19:28:52 +0300

    Initial commit

---
+++ src/main/java/com/nikego/skycapitals/SkycapitalsApplication.java
@@ -0,0 +1,13 @@
+package com.nikego.skycapitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkycapitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkycapitalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikego/skycapitals/SkycapitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikego.skycapitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkycapitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
