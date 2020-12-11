044a4f02d583e3ca69098e8e446bde39db0521a4
nvvi9 <nvvi9@protonmail.com>
2020-12-11 18:50:54 +0300

    balance motion layout

--- skycapitalsmobile/src/main/java/com/nikego/skycapitals/network/SkyCapitalsDataSource.kt
+++ skycapitalsmobile/src/main/java/com/nikego/skycapitals/network/SkyCapitalsDataSource.kt
@@ -1,35 +1,15 @@
 package com.nikego.skycapitals.network
 
 import android.util.Log
-import com.nikego.skycapitals.data.*
+import com.nikego.skycapitals.data.CardType
 import com.nikego.skycapitals.data.datatype.Result
 import com.nikego.skycapitals.data.skycapitalsserver.UserLogin
 import com.nikego.skycapitals.data.skycapitalsserver.UserRegister
 import com.nikego.skycapitals.network.retrofit.SkyCapitalsApiService
 import javax.inject.Inject
-import kotlin.random.Random
 
 
 class SkyCapitalsDataSource @Inject constructor(private val skyCapitalsApiService: SkyCapitalsApiService) {
-
-    private val user = User(
-        userId = Random.nextInt(),
-        email = "sobaka@gmail.ru",
-        userName = "Pitro",
-        userSurname = "Pitrov",
-        password = 1234,
-        scores = List(15) {
-            Score(
-                Random.nextInt(),
-                listOf(Currency.USD, Currency.BYN, Currency.EUR).random(),
-                getBankCards()
-            )
-        }
-    )
-
-    private fun getBankCards() = List((1..5).random()) {
-        BankCard(Random.nextInt(), "card name", Random.nextInt(0, 1000))
-    }
 
     suspend fun login(userLogin: UserLogin) =
         try {
